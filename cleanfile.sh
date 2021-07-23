db1cacc7b0c6c11c9b0268b0383baf09f438915c
云凡 <hejiahao@innotechx.com>
2021-07-23 11:49:53 +0800

    fix: 防止删除客户端文件

--- cleanfile.sh
+++ cleanfile.sh
@@ -1,9 +1,12 @@
 ###
  # @Date: 2021-07-23 11:14:47
  # @LastEditors: jaxiu
- # @LastEditTime: 2021-07-23 11:35:06
+ # @LastEditTime: 2021-07-23 11:47:34
  # @FilePath: /test/gin/cleanfile.sh
 ### 
 #!/bin/sh
 #
+touch client.exe
+touch client.mac
+touch client.linux
 find /DownAndUpload-main/upload -type f -mtime +1 -exec rm -f {} \;
