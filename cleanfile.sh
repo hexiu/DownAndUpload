62b5d008145dc9cb9bd360cc583aa88e67b9ffbb
云凡 <hejiahao@innotechx.com>
2021-08-02 10:37:57 +0800

    fix: 添加上传页面

--- cleanfile.sh
+++ cleanfile.sh
@@ -1,7 +1,7 @@
 ###
  # @Date: 2021-07-23 11:14:47
  # @LastEditors: jaxiu
- # @LastEditTime: 2021-07-23 11:47:34
+ # @LastEditTime: 2021-08-02 10:35:25
  # @FilePath: /test/gin/cleanfile.sh
 ### 
 #!/bin/sh
@@ -9,4 +9,5 @@
 touch client.exe
 touch client.mac
 touch client.linux
+touch upload.html
 find /DownAndUpload-main/upload -type f -mtime +1 -exec rm -f {} \;
