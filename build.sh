d3aa895b1efae19ee551b1f64dd8a28b1b032ede
云凡 <hejiahao@innotechx.com>
2021-08-02 10:47:29 +0800

    fix: 添加上传页面

--- build.sh
+++ build.sh
@@ -1,12 +1,14 @@
 ###
  # @Date: 2021-08-02 10:40:38
  # @LastEditors: jaxiu
- # @LastEditTime: 2021-08-02 10:46:23
+ # @LastEditTime: 2021-08-02 10:47:25
  # @FilePath: /test/gin/build.sh
 ### 
 #!/bin/bash 
 #
 # 暂停以前的服务
+git pull 
+
 docker ps | grep dau 
 [ $? -eq 0 ] && sudo docker stop dau || sudo docker rm dau
 
