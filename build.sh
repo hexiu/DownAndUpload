8ca819b2a6dedf6b643b5419ab56ccc87a7364b0
云凡 <hejiahao@innotechx.com>
2021-08-02 10:46:47 +0800

    fix: 添加上传页面

---
+++ build.sh
@@ -0,0 +1,21 @@
+###
+ # @Date: 2021-08-02 10:40:38
+ # @LastEditors: jaxiu
+ # @LastEditTime: 2021-08-02 10:46:23
+ # @FilePath: /test/gin/build.sh
+### 
+#!/bin/bash 
+#
+# 暂停以前的服务
+docker ps | grep dau 
+[ $? -eq 0 ] && sudo docker stop dau || sudo docker rm dau
+
+docker images | grep downandupload 
+[ $? -eq 0 ] && sudo docker rmi downandupload
+
+docker build -t downandupload:v0.0.1 .
+docker run  -dti --name dau -p 8081:8081 downandupload:v0.0.1
+
+docker ps | grep dau 
+[ $? -eq 0 ] && echo "dau running ..."
+
