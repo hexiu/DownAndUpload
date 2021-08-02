1d01d04d9e030dd889471922b8e2686a9aae0a72
云凡 <hejiahao@innotechx.com>
2021-08-02 10:57:13 +0800

    fix: 新增快捷发布脚本

--- build.sh
+++ build.sh
@@ -1,7 +1,7 @@
 ###
  # @Date: 2021-08-02 10:40:38
  # @LastEditors: jaxiu
- # @LastEditTime: 2021-08-02 10:47:25
+ # @LastEditTime: 2021-08-02 10:49:39
  # @FilePath: /test/gin/build.sh
 ### 
 #!/bin/bash 
@@ -10,14 +10,15 @@
 git pull 
 
 docker ps | grep dau 
-[ $? -eq 0 ] && sudo docker stop dau || sudo docker rm dau
+[ $? -eq 0 ] && docker stop dau && docker rm dau
 
 docker images | grep downandupload 
-[ $? -eq 0 ] && sudo docker rmi downandupload
+[ $? -eq 0 ] && docker rmi downandupload:v0.0.1
 
 docker build -t downandupload:v0.0.1 .
 docker run  -dti --name dau -p 8081:8081 downandupload:v0.0.1
 
 docker ps | grep dau 
 [ $? -eq 0 ] && echo "dau running ..."
+
 
